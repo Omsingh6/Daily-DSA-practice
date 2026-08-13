9daa6c2df3ba0c8048f9456bcce0dc33935cb530
Omsingh6 <bandwalom@gmail.com>
2026-08-13 18:44:39 +0530

    First commit

---
+++ 1_Arrays/problemA1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class problemA1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        System.out.println("The elements of the array are:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        
+        int largest = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        
+        int sl = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > sl && arr[i] != largest) {
+                sl = arr[i];
+            }
+        }
+
+        int Tl =0;
+
+        for(int i=0; i< size; i++){
+          if(arr[i] > Tl && arr[i] != sl &&  arr[i] != largest ){
+            Tl = arr[i];
+          }
+        }
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Second Largest = " + sl);
+        System.out.println("Tl=" + Tl);
+
+        sc.close();
+    }
+}
