f2e15d132fb0c0795aabadee8ab0fc5ebfe3ca26
Omsingh6 <bandwalom@gmail.com>
2026-08-25 12:27:08 +0530

    Add Sum of array

---
+++ 1_Arrays/SumOfArray.java
@@ -0,0 +1,34 @@
+// Find the sum of arrray .
+
+import java.util.Scanner;
+
+class SumOfArray {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+     
+     System.out.println("Enter thr size of array:-");
+     int size = sc.nextInt();
+
+     int[] arr = new int[size];
+
+     System.out.println("Enter the element of array:-");
+        for(int i=0; i<size; i++){
+        arr[i] = sc.nextInt();
+      }
+
+      System.out.println("The array is :-");
+      for(int i=0; i<size; i++){
+        System.out.print(arr[i]+" ");
+      }
+ 
+      int sum =0;
+      for(int i=0; i<size; i++){
+        sum += arr[i];
+      }
+
+      System.out.println("\n" + sum);
+
+
+     
+  }
+}
